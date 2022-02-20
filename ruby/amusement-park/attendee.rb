36c4b1d448d76e70b2bfef391c09052189f13418
JohannesHupfeld <j.hupfeld@gmail.com>
2022-02-19 22:28:42 -0500

    solution

--- ruby/amusement-park/attendee.rb
+++ ruby/amusement-park/attendee.rb
@@ -1,21 +1,24 @@
 class Attendee
+  attr_reader :height, :pass_id
+
   def initialize(height)
-    raise 'Implement the Attendee#initialize method'
+    # raise 'Implement the Attendee#initialize method'
+    @height = height
+    revoke_pass!
   end
 
   def height
-    raise 'Implement the Attendee#height method'
-  end
-
-  def pass_id
-    raise 'Implement the Attendee#pass_id method'
+    # raise 'Implement the Attendee#height method'
+    @height = 100
   end
 
   def issue_pass!(pass_id)
-    raise 'Implement the Attendee#issue_pass! method'
+    # raise 'Implement the Attendee#issue_pass! method'
+    @pass_id = pass_id
   end
 
   def revoke_pass!
-    raise 'Implement the Attendee#revoke_pass! method'
+    # raise 'Implement the Attendee#revoke_pass! method'
+    @pass_id = nil
   end
 end
